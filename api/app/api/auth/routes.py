b3ca1fc3fcb5a8fc0a10591f2175f2645fb3f1fd
Pedro Martins <pedro.martins@semeq.com>
2025-05-27 09:55:16 -0300

    feat: added schema module and added it in routes arguments

--- api/app/api/auth/routes.py
+++ api/app/api/auth/routes.py
@@ -1,19 +1,21 @@
 from fastapi import APIRouter
 
+from app.api.auth.schema import LoginRequest, RefreshTokenRequest, VerifyTokenRequest
+
 
 router = APIRouter()
 
 
 @router.post("/login")
-def login(data):
+def login(data: LoginRequest):
     return {"Login": "Placeholder"}
 
 
 @router.post("/token/verify")
-def verify_token(data):
+def verify_token(data: VerifyTokenRequest):
     return {"TokenVerify": "Placeholder"}
 
 
 @router.post("/token/refresh")
-def refresh_token(data):
+def refresh_token(data: RefreshTokenRequest):
     return {"TokenRefresh": "Placeholder"}
