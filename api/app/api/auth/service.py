3b9d7f3ec126ee2d47cd37bd942753539d10d0fa
Pedro Martins <pedro.martins@semeq.com>
2025-05-27 11:21:42 -0300

    feat(api/auth/service): add timeout setup to requests

--- api/app/api/auth/service.py
+++ api/app/api/auth/service.py
@@ -4,11 +4,16 @@ from app.api.auth.schema import LoginRequest, VerifyTokenRequest, RefreshTokenRe
 
 
 class AuthService:
+    LOGIN_SERVICE_TIMEOUT: float = 5.0
+    VERIFY_TOKEN_TIMEOUT: float = 5.0
+    REFRESH_TOKEN_TIMEOUT: float = 5.0
+
     def login_user(self, credentials: LoginRequest):
         return handle_http_request(
             method="POST",
             url=f"{settings.external_api_url}/token",
             json=credentials.model_dump(),
+            timeout=self.LOGIN_SERVICE_TIMEOUT,
         )
 
     def verify_token(self, token: VerifyTokenRequest):
@@ -16,6 +21,7 @@ class AuthService:
             method="POST",
             url=f"{settings.external_api_url}/token/verify",
             json=token.model_dump(),
+            timeout=self.VERIFY_TOKEN_TIMEOUT,
         )
 
     def refresh_token(self, token: RefreshTokenRequest):
@@ -23,4 +29,5 @@ class AuthService:
             method="POST",
             url=f"{settings.external_api_url}/token/refresh",
             json=token.model_dump(),
+            timeout=self.REFRESH_TOKEN_TIMEOUT,
         )
