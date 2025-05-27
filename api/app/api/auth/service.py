acbf06cfc65b916c057117228acad7e85e6faef6
Pedro Martins <pedro.martins@semeq.com>
2025-05-27 11:16:08 -0300

    feat(api/core): add http_proxy module for forwarding status and message from upstream APIs

--- api/app/api/auth/service.py
+++ api/app/api/auth/service.py
@@ -1,37 +1,19 @@
-import httpx
-from fastapi import HTTPException
-
+from app.core.http_proxy import handle_http_request
 from app.core.config import settings
 from app.api.auth.schema import LoginRequest, VerifyTokenRequest, RefreshTokenRequest
 
 
 class AuthService:
     def login_user(self, credentials: LoginRequest):
-        try:
-            response = httpx.request(
-                method="POST",
-                url=f"{settings.external_api_url}/token",
-                json=credentials.model_dump(),
-            )
-
-            response.raise_for_status()
-            return response.json()
-
-        except Exception as e:
-            raise HTTPException(
-                status_code=503, detail="Service Unavailable") from e
+        return handle_http_request(
+            method="POST",
+            url=f"{settings.external_api_url}/token",
+            json=credentials.model_dump(),
+        )
 
     def verify_token(self, token: VerifyTokenRequest):
-        try:
-            response = httpx.request(
-                method="POST",
-                url=f"{settings.external_api_url}/token/verify",
-                json=token.model_dump(),
-            )
-
-            response.raise_for_status()
-            return response.json()
-
-        except Exception as e:
-            raise HTTPException(
-                status_code=503, detail="Service Unavailable") from e
+        return handle_http_request(
+            method="POST",
+            url=f"{settings.external_api_url}/token/verify",
+            json=token.model_dump(),
+        )
