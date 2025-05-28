c980f4a28fc6f9591aaa16bb7bba579fe7965984
Pedro Martins <pedro.martins@semeq.com>
2025-05-28 09:46:47 -0300

    feat(tui/screens/main-screen): added main screen

--- tui/src/app/proxy_tui.py
+++ tui/src/app/proxy_tui.py
@@ -2,6 +2,7 @@ from textual.app import App
 
 from src.globals import api_client
 from src.screens.connection_screen import ConnectionScreen
+from src.screens.main_screen import MainScreen
 
 
 class ProxyTUI(App):
@@ -9,8 +10,9 @@ class ProxyTUI(App):
         api_client.app = self
 
         self.install_screen(ConnectionScreen(), name="connection_screen")
+        self.install_screen(MainScreen(api_client), name="main_screen")
 
         api_client.request_connection()
 
     def on_successful_login(self) -> None:
-        self.notify("Navigate to main screen...")
+        self.push_screen("main_screen")
